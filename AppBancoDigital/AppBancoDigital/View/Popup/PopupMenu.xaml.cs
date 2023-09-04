4539d7788ef8824b2bef95474428ebe613a4f972
moraisbianca <biancacamilamorais@gmail.com>
2023-09-04 15:42:58 -0300

    Estou trabalhando no menu da home, tentando deixar ele decente

--- AppBancoDigital/AppBancoDigital/View/Popup/PopupMenu.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Popup/PopupMenu.xaml.cs
@@ -16,6 +16,13 @@ namespace AppBancoDigital.View.Popup
         public PopupMenu()
         {
             InitializeComponent();
+            Barrinha.Source = ImageSource.FromResource("AppBancoDigital.Images.barrinha.png");
+            btn_setinha_1.Source = ImageSource.FromResource("AppBancoDigital.Images.seta-direita.png");
+            btn_setinha_2.Source = ImageSource.FromResource("AppBancoDigital.Images.seta-direita.png");
+            btn_setinha_3.Source = ImageSource.FromResource("AppBancoDigital.Images.seta-direita.png");
+            btn_avatar.Source = ImageSource.FromResource("AppBancoDigital.Images.avatar.png");
+            btn_notificacoes.Source = ImageSource.FromResource("AppBancoDigital.Images.notificacao.png");
+            btn_deslogar.Source = ImageSource.FromResource("AppBancoDigital.Images.sair.png");
 
             string[] resultsArray = explode(" ", App.DadosCorrentista.Nome);
             string nome = resultsArray[0];
@@ -25,6 +32,21 @@ namespace AppBancoDigital.View.Popup
         public static string[] explode(string separator, string source)
         {
             return source.Split(new string[] { separator }, StringSplitOptions.None);
+        }
+
+        private void btn_avatar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_notificacoes_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_deslogar_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
