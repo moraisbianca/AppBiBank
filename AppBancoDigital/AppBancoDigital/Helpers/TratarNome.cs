55238f7b59846f656e408ac47e7e4c53e03d39e2
moraisbianca <biancacamilamorais@gmail.com>
2023-09-18 09:35:30 -0300

    Saldo funcionando 👍✔

---
+++ AppBancoDigital/AppBancoDigital/Helpers/TratarNome.cs
@@ -0,0 +1,12 @@
+﻿using AppBancoDigital.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Helpers
+{
+    public class TratarNome
+    {
+        //Fazer função para pegar primeiro e ultimo nome e abreviar os demais
+    }
+}
